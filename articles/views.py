46d8ff1107e4aed82aeafad5ddd6f6e25b5fc554
kmg0485 <kmg0485@naver.com>
2022-09-23 22:12:33 +0900

    Add menu and name

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
+import random
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menus)
+    context = {
+        'pick' : pick,
+        'name' : name,
+    }
+    
+    return render(request, 'dinner.html', context)
