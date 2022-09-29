545091e083e3ba08da11033f9eeea9e0dc641ab4
kmg0485 <kmg0485@naver.com>
2022-09-29 21:22:20 +0900

    update

--- articles/views.py
+++ articles/views.py
@@ -44,3 +44,18 @@ def delete(request, pk):
     if request.method == "POST":
         article.delete() 
     return redirect('articles:dinner')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context={
+        'article' : article,
+    }
+    return render(request, 'edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
