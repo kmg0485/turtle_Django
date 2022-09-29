46757ac821b379daaaade835086cd1b6c557085c
kmg0485 <kmg0485@naver.com>
2022-09-29 20:43:13 +0900

    details

--- articles/views.py
+++ articles/views.py
@@ -5,13 +5,12 @@ from .models import Article
 def index(request):
     return render(request, 'index.html')
 
-def dinner(request, name):
+def dinner(request):
     menus = [{"name":'족발', "price":30000}, {"name":'햄버거', "price":5000}, {"name":'치킨', "price":20000}, {"name":'초밥', "price":15000}]
     pick = random.choice(menus)
     articles = Article.objects.order_by('-pk')
     context = {
         'pick' : pick,
-        'name' : name,
         'menus' : menus,
         'articles' : articles,
     }
@@ -28,4 +27,14 @@ def create_review(request):
     article = Article(title=title, content=content)
     article.save()
 
-    return redirect('articles/dinner/무언가/')
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context={
+        'article' : article,
+    }
+    return render(request, 'detail.html', context)
+    
+    
