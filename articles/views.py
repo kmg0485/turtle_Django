8c5f32d33c83a5b585dca0d9bba954e28683da86
kmg0485 <kmg0485@naver.com>
2022-09-29 20:54:32 +0900

    deletepost

--- articles/views.py
+++ articles/views.py
@@ -40,5 +40,7 @@ def detail(request, pk):
     
 def delete(request, pk):
     article = Article.objects.get(pk=pk)
-    article.delete() 
+    
+    if request.method == "POST":
+        article.delete() 
     return redirect('articles:dinner')
